33e6270a7e497051913301bdd339647a63386b92
+ <+>
2022-06-10 09:51:14 +0300

    commit on master

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper ';
+
+
+
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+
+"const currentUser = 'Grace Hopper';".length;
+//=> 35
+
+currentUser;
+//=> Uncaught ReferenceError: currentUser is not defined
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
